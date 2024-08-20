e734b9acbb048aa90b4f95266575124cf17486aa
5dma <mark@lautman.net>
2024-08-20 15:30:32 -0400

    Spaces to tabs

--- main.c
+++ main.c
@@ -8,102 +8,102 @@
 /* https://imagemagick.org/api/drawing-wand.php */
 /* https://imagemagick.org/api/pixel-wand.php */
 int main(int argc, char *argv[]) {
-    Settings *settings;
-    settings = read_json();
-    if (settings == NULL) {
-        return 0;
-    }
+	Settings *settings;
+	settings = read_json();
+	if (settings == NULL) {
+		return 0;
+	}
 
-    MagickWand *m_wand = NULL;
+	MagickWand *m_wand = NULL;
 
-    MagickWandGenesis();
-    m_wand = NewMagickWand();
+	MagickWandGenesis();
+	m_wand = NewMagickWand();
 
-    MagickBooleanType result = MagickReadImage(m_wand, "/home/abba/Desktop/leo-tolstoy-21.jpg");
+	MagickBooleanType result = MagickReadImage(m_wand, "/home/abba/Desktop/leo-tolstoy-21.jpg");
 
-    if (result == MagickFalse) {
-        g_print("Could not read the image %s. Exiting\n", "/home/abba/Desktop/leo-tolstoy-21.jpg");
-        return -1;
-    }
+	if (result == MagickFalse) {
+		g_print("Could not read the image %s. Exiting\n", "/home/abba/Desktop/leo-tolstoy-21.jpg");
+		return -1;
+	}
 
-    Annotation *annotation;
-    annotation = read_annotation(settings);
-    if (annotation == NULL) {
-        return 0;
-    }
-    g_print("The annotation text is %s\n", annotation->text_string);
+	Annotation *annotation;
+	annotation = read_annotation(settings);
+	if (annotation == NULL) {
+		return 0;
+	}
+	g_print("The annotation text is %s\n", annotation->text_string);
 
-    /* Resize the image */
-    resize(m_wand, settings, annotation);
+	/* Resize the image */
+	resize(m_wand, settings, annotation);
 
-    DrawingWand *d_wand = NewDrawingWand();
-    PixelWand *p_wand = NewPixelWand();
-    PixelSetColor(p_wand, settings->balloon_stroke_color);
+	DrawingWand *d_wand = NewDrawingWand();
+	PixelWand *p_wand = NewPixelWand();
+	PixelSetColor(p_wand, settings->balloon_stroke_color);
 
-    PixelSetAlpha(p_wand, 1.0);
+	PixelSetAlpha(p_wand, 1.0);
 
-    DrawSetStrokeColor(d_wand, p_wand);
-    DrawSetStrokeWidth(d_wand, settings->stroke_width);
-    DrawSetStrokeOpacity(d_wand, 1.0);
-    DrawSetFontSize(d_wand, settings->font_size);
-    DrawSetFontFamily(d_wand, settings->font);
+	DrawSetStrokeColor(d_wand, p_wand);
+	DrawSetStrokeWidth(d_wand, settings->stroke_width);
+	DrawSetStrokeOpacity(d_wand, 1.0);
+	DrawSetFontSize(d_wand, settings->font_size);
+	DrawSetFontFamily(d_wand, settings->font);
 
-    // DrawLine(d_wand, 20, 20, 300, 100);
+	// DrawLine(d_wand, 20, 20, 300, 100);
 
-    PixelWand *p_wand_text = NewPixelWand();
-    DrawingWand *d_wand_text = NewDrawingWand();
-    PixelSetColor(p_wand_text, settings->balloon_stroke_color);
+	PixelWand *p_wand_text = NewPixelWand();
+	DrawingWand *d_wand_text = NewDrawingWand();
+	PixelSetColor(p_wand_text, settings->balloon_stroke_color);
 
-    PixelSetAlpha(p_wand_text, 1.0);
+	PixelSetAlpha(p_wand_text, 1.0);
 
-    DrawSetStrokeColor(d_wand_text, p_wand_text);
-    DrawSetStrokeWidth(d_wand_text, settings->stroke_width);
-    DrawSetStrokeOpacity(d_wand_text, 1.0);
-    DrawSetFontSize(d_wand_text, settings->font_size);
+	DrawSetStrokeColor(d_wand_text, p_wand_text);
+	DrawSetStrokeWidth(d_wand_text, settings->stroke_width);
+	DrawSetStrokeOpacity(d_wand_text, 1.0);
+	DrawSetFontSize(d_wand_text, settings->font_size);
 
-    result = add_text(m_wand, d_wand_text, settings, annotation);
+	result = add_text(m_wand, d_wand_text, settings, annotation);
 
-    result = MagickAnnotateImage(m_wand, d_wand_text, 300, 300, 0, "I REALLY AM GOING TO BARF");
+	result = MagickAnnotateImage(m_wand, d_wand_text, 300, 300, 0, "I REALLY AM GOING TO BARF");
 
-    PixelSetColor(p_wand, settings->balloon_fill_color);
-    DrawSetFillColor(d_wand, p_wand);
-    DrawRectangle(d_wand, 20, 20, 100, 100);
+	PixelSetColor(p_wand, settings->balloon_fill_color);
+	DrawSetFillColor(d_wand, p_wand);
+	DrawRectangle(d_wand, 20, 20, 100, 100);
 
-    result = PixelSetColor(p_wand, "#ff0000");
-    DrawSetFillColor(d_wand, p_wand);
-    add_balloon(m_wand, d_wand, settings, annotation);
+	result = PixelSetColor(p_wand, "#ff0000");
+	DrawSetFillColor(d_wand, p_wand);
+	add_balloon(m_wand, d_wand, settings, annotation);
 
-    // DrawingWand *d_wand_text = NewDrawingWand();
+	// DrawingWand *d_wand_text = NewDrawingWand();
 
-    //	PixelWand *p_wand_text = NewPixelWand();
-    PixelSetColor(p_wand_text, settings->balloon_stroke_color);
+	//	PixelWand *p_wand_text = NewPixelWand();
+	PixelSetColor(p_wand_text, settings->balloon_stroke_color);
 
-    PixelSetAlpha(p_wand_text, 1.0);
+	PixelSetAlpha(p_wand_text, 1.0);
 
-    DrawSetStrokeColor(d_wand_text, p_wand_text);
-    DrawSetStrokeWidth(d_wand_text, settings->stroke_width);
-    DrawSetStrokeOpacity(d_wand_text, 1.0);
-    DrawSetFontSize(d_wand_text, settings->font_size);
+	DrawSetStrokeColor(d_wand_text, p_wand_text);
+	DrawSetStrokeWidth(d_wand_text, settings->stroke_width);
+	DrawSetStrokeOpacity(d_wand_text, 1.0);
+	DrawSetFontSize(d_wand_text, settings->font_size);
 
-    result = add_text(m_wand, d_wand_text, settings, annotation);
+	result = add_text(m_wand, d_wand_text, settings, annotation);
 
-    MagickAnnotateImage(m_wand, d_wand_text, 300, 300, 0, "I REALLY AM GOING TO BARF");
+	MagickAnnotateImage(m_wand, d_wand_text, 300, 300, 0, "I REALLY AM GOING TO BARF");
 
-    MagickDrawImage(m_wand, d_wand);
+	MagickDrawImage(m_wand, d_wand);
 
-    /* Write the new image */
-    MagickWriteImage(m_wand, settings->new_image_path);
+	/* Write the new image */
+	MagickWriteImage(m_wand, settings->new_image_path);
 
-    /* Clean up */
-    DestroyPixelWand(p_wand);
-    DestroyDrawingWand(d_wand);
-    if (m_wand)
-        m_wand = DestroyMagickWand(m_wand);
+	/* Clean up */
+	DestroyPixelWand(p_wand);
+	DestroyDrawingWand(d_wand);
+	if (m_wand)
+		m_wand = DestroyMagickWand(m_wand);
 
-    MagickWandTerminus();
-    g_print("The new image is at %s\n", settings->new_image_path);
-    g_free(settings);
-    g_free(annotation);
+	MagickWandTerminus();
+	g_print("The new image is at %s\n", settings->new_image_path);
+	g_free(settings);
+	g_free(annotation);
 
-    return 0;
+	return 0;
 }
