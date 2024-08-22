be20ca1db8ad77fcb1d2ecaf3ac40b8551c1d01c
5dma <mark@lautman.net>
2024-08-21 20:35:56 -0400

    Defined four corners of balloon

--- add_balloon.c
+++ add_balloon.c
@@ -7,21 +7,6 @@
 void add_balloon(MagickWand *m_wand, Settings *settings, Annotation *annotation, Text_Analysis *text_analysis)
 {
 
-	gint64 left = annotation->text_bottom_left.x * annotation->resize_proportion_x - settings->padding;
-	gint64 top = annotation->text_bottom_left.y * annotation->resize_proportion_y - 20; //- annotation->
-
-	/*gint64 excess_spacing = text_height - (text_analysis->number_text_lines * settings->font_size );
-	gint64 partial_excess_spacing = excess_spacing * 0.75;
-	gint64 baseline = \
-		(annotation->text_bottom_left.y * annotation->resize_proportion_y) - \
-		(text_analysis->number_text_lines * settings->font_size) - \
-		partial_excess_spacing;
-
-	
-
-*/
-
-
 	DrawingWand *d_wand = NewDrawingWand();
 	PixelWand *p_wand = NewPixelWand();
 	PixelSetColor(p_wand, settings->balloon_stroke_color);
@@ -39,13 +24,21 @@ void add_balloon(MagickWand *m_wand, Settings *settings, Annotation *annotation,
 		settings->stroke_width ;
 	
 	gint64 top_left_y = \
-		annotation->text_bottom_left.y - \
+		annotation->text_bottom_left.y * annotation->resize_proportion_y - \
 		text_analysis->text_height - \
 		settings->padding -\
 		settings->stroke_width;
+
+	gint64 bottom_right_x = top_left_x + \
+		text_analysis->text_width + \
+		settings->padding * 2 + \
+		settings->stroke_width * 2;
+	gint64 bottom_right_y = \
+		top_left_y + \
+		text_analysis->text_height + \
+		settings->padding * 2 + \
+		settings->stroke_width * 2;
 		
-	gint64 bottom_right_x = top_left_x + text_analysis->text_width;
-	gint64 bottom_right_y = top_left_y + text_analysis->text_height;
 	DrawRectangle(d_wand, top_left_x, top_left_y, bottom_right_x, bottom_right_y);
 	MagickDrawImage(m_wand, d_wand);
 
