e4ae059818e25d6e8a21ded54d222c8f87af3ef3
5dma <mark@lautman.net>
2024-09-12 12:54:05 -0400

    Adds all remaining controls to Annotation box

--- build_gui.c
+++ build_gui.c
@@ -78,9 +78,43 @@ void activate(GtkApplication *app, gpointer user_data) {
 	gtk_grid_attach ( GTK_GRID(grid_coordinates), btn_point_text_bottom, 1, 3, 1, 1);
 	gtk_grid_attach ( GTK_GRID(grid_coordinates), btn_point_vertex, 2, 3, 1, 1);
 
+	/* Controls for exported image's width and theme selection. */
+
+	GtkWidget * box_width_theme  = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+	GtkWidget * lbl_new_width = gtk_label_new ("New width:");
+	GtkWidget * lbl_theme = gtk_label_new ("Theme:");
+	GtkWidget * spin_new_width = gtk_spin_button_new (adjustment_coordinates, 2, 4);
+
+	const char boy[] = "Boy";
+	const char girl[] = "Girl";
+	const char * theme_names[] = {boy, girl, NULL};
+ 	GtkWidget * dropdown_theme = gtk_drop_down_new_from_strings (theme_names);
+
+
+	/* Controls for text string and export button. */
+
+	GtkWidget * grid_text_string_export = gtk_grid_new ();
+	GtkWidget * lbl_text_string = gtk_label_new ("Text string:");
+	GtkWidget * entry_text_string = gtk_entry_new();
+	GtkWidget * btn_export = gtk_button_new_with_label("Export");
+
+	gtk_grid_attach ( GTK_GRID(grid_text_string_export), lbl_text_string, 0,0,1,2);
+	gtk_grid_attach ( GTK_GRID(grid_text_string_export), entry_text_string, 0,1,1,1);
+	gtk_grid_attach ( GTK_GRID(grid_text_string_export), btn_export, 1,1,1,1);
+
+	gtk_box_append(GTK_BOX(box_width_theme), lbl_new_width);
+	gtk_box_append(GTK_BOX(box_width_theme), spin_new_width);
+	gtk_box_append(GTK_BOX(box_width_theme), lbl_theme);
+	gtk_box_append(GTK_BOX(box_width_theme), dropdown_theme);
+
+	GtkWidget* picture_preview = gtk_picture_new_for_filename ("/home/abba/Ronen_Letters/graphic_quotes/knesset.png");
 
 	gtk_box_append(GTK_BOX(box_annotation), grid_annotation);
 	gtk_box_append(GTK_BOX(box_annotation), grid_coordinates);
+	gtk_box_append(GTK_BOX(box_annotation), box_width_theme);
+	gtk_box_append(GTK_BOX(box_annotation), grid_text_string_export);
+	gtk_box_append(GTK_BOX(box_annotation), picture_preview);
+
 
 	GtkWidget *status_bar = gtk_entry_new();
 	gtk_entry_set_placeholder_text(GTK_ENTRY(status_bar), "Ready...");
