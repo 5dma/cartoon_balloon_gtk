ba32fc98635afd58e6447d9db839326eff59cc79
5dma <mark@lautman.net>
2024-10-16 11:36:53 -0400

    Add function that converts hex color to decimal 1..100

--- build_controls/build_controllers_theme.c
+++ build_controls/build_controllers_theme.c
@@ -1,9 +1,76 @@
 #include "../headers.h"
+#include <ctype.h>
 
 /**
  * @file build_controllers_theme.c
  * @brief Contains functions for adding callbacks to the themes window.
  */
+
+
+gint convert_hex_to_int(const gchar digit) {
+
+	switch (toupper(digit)) {
+		case '0':
+			return 0;
+		case '1':
+			return 1;
+		case '2':
+			return 2;
+		case '3':
+			return 3;
+		case '4':
+			return 4;
+		case '5':
+			return 5;
+		case '6':
+			return 6;
+		case '7':
+			return 7;
+		case '8':
+			return 8;
+		case '9':
+			return 9;
+		case 'A':
+			return 10;
+		case 'B':
+			return 11;
+		case 'C':
+			return 12;
+		case 'D':
+			return 13;
+		case 'E':
+			return 14;
+		case 'F':
+			return 15;
+		default:
+			return 0;
+	}
+	return 0;
+
+}
+
+void convert_hex_to_rgb(float *rgb, gchar *hex) {
+
+	gchar *r_hex = g_strndup (hex + 1, 2);
+ 	gchar *g_hex =  g_strndup (hex + 3, 2);
+	gchar *b_hex =   g_strndup (hex + 5, 2);
+
+	g_print("red: %s\n", r_hex);
+	g_print("green: %s\n", g_hex);
+	g_print("blue: %s\n", b_hex);
+	
+	int trash1 = convert_hex_to_int(r_hex[0]);
+	int trash2 = convert_hex_to_int(r_hex[1]);
+	int trash = 16*trash1 + trash2;
+	float normalized = (float) trash / 256;
+
+	g_print("r: %d, g: %d, b: %d, %f\n", trash1, trash2, trash, normalized);
+
+	g_free(r_hex);
+	g_free(g_hex);
+	g_free(b_hex);
+
+}
 
 void theme_selection_changed(GObject *self, GParamSpec *pspec, gpointer data) {
 	User_Data *user_data = (User_Data *)data;
@@ -15,10 +82,13 @@ void theme_selection_changed(GObject *self, GParamSpec *pspec, gpointer data) {
 	const char *selected_theme_name = gtk_string_list_get_string(GTK_STRING_LIST(model_theme), selected_item);
 
 	int new_compare = g_strcmp0(NEW_THEME, selected_theme_name);
+
 	if (new_compare == 0) {
+		/* If the user selected (new), enable the entry box for the new theme name.*/
 		gtk_widget_set_sensitive(gui_data_theme->entry_new_theme, TRUE);
 		gtk_widget_grab_focus(gui_data_theme->entry_new_theme);
 	} else {
+		/* Otherwise, disable the entry box and set the controls to the values for the selected theme. */
 		gtk_widget_set_sensitive(gui_data_theme->entry_new_theme, FALSE);
 		Theme *theme = (Theme *)g_hash_table_lookup(user_data->theme_hash, selected_theme_name);
 
@@ -37,6 +107,20 @@ void theme_selection_changed(GObject *self, GParamSpec *pspec, gpointer data) {
 		gtk_entry_buffer_set_text(entry_buffer, theme->balloon_stroke_color, -1);
 
 		gtk_spin_button_set_value(GTK_SPIN_BUTTON(gui_data_theme->spin_stroke_width), theme->stroke_width);
+
+		Theme_Preview *theme_preview = user_data->theme_preview;
+		theme_preview->selected_theme = theme;
+
+		convert_hex_to_rgb(theme_preview->fill_rgb, theme->balloon_fill_color);
+		convert_hex_to_rgb(theme_preview->stroke_rgb, theme->balloon_stroke_color);
+
+	/*theme_preview->fill_rgb[0]= 0.44;
+	theme_preview->fill_rgb[1]= 0.62;
+	theme_preview->fill_rgb[2]= 0.81;
+
+	theme_preview->stroke_rgb[0]= 0.02;
+	theme_preview->stroke_rgb[1]= 0.0;
+	theme_preview->stroke_rgb[2]= 0.98; */
 	}
 }
 
@@ -117,14 +201,6 @@ void build_controllers_theme(User_Data *user_data) {
 
 	theme_preview->vertex_right.x = midpoint_horizontal + 10;
 	theme_preview->vertex_right.y = elevation;
-
-	theme_preview->fill_rgb[0]= 0.44;
-	theme_preview->fill_rgb[1]= 0.62;
-	theme_preview->fill_rgb[2]= 0.81;
-
-	theme_preview->stroke_rgb[0]= 0.02;
-	theme_preview->stroke_rgb[1]= 0.0;
-	theme_preview->stroke_rgb[2]= 0.98;
 
 	theme_preview->text_start.x = midpoint_horizontal - 20;
 	theme_preview->text_start.y = midpoint_vertical;
