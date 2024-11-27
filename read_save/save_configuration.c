105e2a8ba81ecd0a1be874b5bb07e365db5c113b
5dma <mark@lautman.net>
2024-11-27 15:56:18 -0500

    Save config values from GUI.

--- read_save/save_configuration.c
+++ read_save/save_configuration.c
@@ -13,25 +13,31 @@
 void save_configuration(User_Data *user_data, JsonBuilder *builder) {
 
 	Configuration *configuration = user_data->configuration;
+	Gui_Data_Configuration *gui_data_configuration = user_data->gui_data->gui_data_configuration;
 
 	json_builder_set_member_name (builder, "configuration");
 
 	json_builder_begin_object (builder);
 	
-	json_builder_set_member_name (builder, "max_annotation_length");
-	json_builder_add_int_value(builder, configuration->max_annotation_length);
+	double config_value  = gtk_spin_button_get_value (GTK_SPIN_BUTTON(gui_data_configuration->spin_max_annotation_length));
+ 	json_builder_set_member_name (builder, "max_annotation_length");
+	json_builder_add_int_value(builder, (guint64) config_value);
 
+	config_value  = gtk_spin_button_get_value (GTK_SPIN_BUTTON(gui_data_configuration->spin_padding));
 	json_builder_set_member_name (builder, "padding");
-	json_builder_add_int_value(builder, configuration->padding);
+	json_builder_add_int_value(builder, (guint64) config_value);
 
+	config_value  = gtk_spin_button_get_value (GTK_SPIN_BUTTON(gui_data_configuration->spin_elevation));
 	json_builder_set_member_name (builder, "elevation");
-	json_builder_add_int_value(builder, configuration->elevation);
+	json_builder_add_int_value(builder, (guint64) config_value);
 
+	config_value  = gtk_spin_button_get_value (GTK_SPIN_BUTTON(gui_data_configuration->spin_space));
 	json_builder_set_member_name (builder, "space");
-	json_builder_add_int_value(builder, configuration->space);
+	json_builder_add_int_value(builder, (guint64) config_value);
 
+	config_value  = gtk_spin_button_get_value (GTK_SPIN_BUTTON(gui_data_configuration->spin_top_margin));
 	json_builder_set_member_name (builder, "top_margin");
-	json_builder_add_int_value(builder, configuration->top_margin);
+	json_builder_add_int_value(builder, (guint64) config_value);
 
 	json_builder_set_member_name (builder, "log_file_path");
 	json_builder_add_string_value(builder, configuration->log_file_path);
